4d598fa13038940a6af94372ec40aecd48d8626e
Rippley777 <ally.rippley@gmail.com>
2022-10-11 12:00:09 -0500

    cleanup and add readme file

---
+++ README.md
@@ -0,0 +1,21 @@
+# RPN Calculator
+
+> A CLI Reverse Polish Notation calculator built in node.js
+
+### Usage
+
+To use this application:
+
+- Clone this repository to your local machine (only if you haven't cloned this before)
+   ```sh
+   git clone https://github.com/rippley777/rpn-calc
+   ```
+
+- Move to `rpn-calc` directory
+    ```sh
+    cd rpn-calc
+    ```
+
+-   ```sh
+    node index.js
+    ```
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const readline = require("readline");
+const readline = require('readline');
 
-const validOperators = ["*", "/", "+", "-"];
+const validOperators = ['*', '/', '+', '-'];
 
 let calculatedValue = null;
 let numbers = [];
 let operators = [];
-let tempValueString = "";
+let tempValueString = '';
 
 let error = null;
 
@@ -15,17 +15,17 @@ function isNumber(value) {
 
 function calculateValue() {
   if (numbers.length === 0) {
-    return console.log('>No calculated value');
+    return console.log('No calculated value');
   }
   if (operators.length === 0) {
-    return console.log(`\n>${numbers[numbers.length - 1]}`);
+    return console.log(`\n${numbers[numbers.length - 1]}`);
   }
-  if (numbers.length - 1 === operators.length) {
-    return console.log(`Error: not enough numbers\n>${numbers[numbers.length -1]}`)
+  if (numbers.length === operators.length) {
+    return console.log(`\nError: Not enough numbers\n${numbers[numbers.length -1]}`)
   }
   while (operators.length > 0) {
     if (numbers.length === 1 && calculatedValue) {
-      error = 'not enough numbers';
+      error = 'Not enough numbers';
       break;
     }
     const operator = operators.shift();
@@ -33,7 +33,7 @@ function calculateValue() {
     const nextValue = numbers.pop();
 
     switch (operator) {
-      case "*": {
+      case '*': {
         const result = currValue
           ? nextValue * currValue
           : nextValue * numbers.pop();
@@ -41,7 +41,7 @@ function calculateValue() {
         numbers.push(calculatedValue);
         break;
       }
-      case "/": {
+      case '/': {
         const result = currValue
           ? nextValue / currValue
           : nextValue / numbers.pop();
@@ -49,7 +49,7 @@ function calculateValue() {
         numbers.push(calculatedValue);
         break;
       }
-      case "+": {
+      case '+': {
         const result = currValue
           ? nextValue + currValue
           : nextValue + numbers.pop();
@@ -57,7 +57,7 @@ function calculateValue() {
         numbers.push(calculatedValue);
         break;
       }
-      case "-": {
+      case '-': {
         const result = currValue
           ? nextValue - currValue
           : numbers.pop() - nextValue;
@@ -69,55 +69,69 @@ function calculateValue() {
         break;
     }
   }
-  if (error) return console.log(`\nError: ${error}\n>${numbers[numbers.length -1]}`);
-  console.log(`>${calculatedValue || 'no calculated value'}`);
+  if (error) return console.log(`\nError: ${error}`);
+  console.log(`\n${calculatedValue}`);
 }
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
-process.stdin.on("keypress", (str, key) => {
-
-  console.log({ calculatedValue, numbers, operators, tempValueString, error })
-  if (key.name == "q" || (key.name == "d" && key.ctrl == true)) {
-    console.log('Application closed.');
-    process.exit(0);
-  } else if (key.name == "c") {
-    tempValueString = '';
-    numbers = [];
-    operators = [];
-    calculatedValue = null;
-    process.stdout.clearLine();
-    readline.cursorTo(process.stdout, 0, null);
-    process.stdout.write('>Reset\n');
-  } else if (key.name == "backspace") {
-    tempValueString = tempValueString.slice(0, -1);
-    process.stdout.clearLine();
-    readline.cursorTo(process.stdout, 0, null);
-    process.stdout.write(tempValueString);
-  } else if (key.name == "return") {
-    const values = tempValueString.split(" ");
-    values.forEach((value) => {
-      if (isNumber(value)) return numbers.push(parseInt(value));
-      value
-        .split("")
-        .filter((v) => validOperators.indexOf(v) >= 0)
-        .forEach((v) => {
-          operators.push(v);
-        });
-    });
-    tempValueString = "";
-    calculateValue();
-  } else {
-    const { sequence } = key;
-    if (isNumber(key.sequence) || (key.sequence == " ") || validOperators.indexOf(sequence) >= 0) {
-      tempValueString += key.sequence;
-      process.stdout.write(key.sequence);
-    } else {
-      process.stdout.write(`${sequence} is not a valid character\n`);
-      process.stdout.write(`${tempValueString}`);
-    }
+process.stdin.on('keypress', (str, key) => {
+  const { name, ctrl, sequence } = key;
+  switch (name) {
+    case 'q': 
+      console.log('Application closed.');
+      process.exit(0);
+      break;
+    case 'd':
+      if (ctrl) {
+        console.log('Application closed.');
+        process.exit(0);
+      } else {
+        //todo: refactor above default 
+        process.stdout.write(`${sequence} is not a valid character\n`);
+        process.stdout.write(`${tempValueString}`);
+      }
+      break;
+    case 'c':
+      tempValueString = '';
+      numbers = [];
+      operators = [];
+      calculatedValue = null;
+      process.stdout.clearLine();
+      readline.cursorTo(process.stdout, 0, null);
+      process.stdout.write('Application reset\n');
+      break;
+    case 'backspace':
+      tempValueString = tempValueString.slice(0, -1);
+      process.stdout.clearLine();
+      readline.cursorTo(process.stdout, 0, null);
+      process.stdout.write(tempValueString);
+      break;
+    case 'return':
+      const values = tempValueString.split(' ');
+      values.forEach((value) => {
+        if (isNumber(value)) return numbers.push(parseInt(value));
+        value
+          .split('')
+          .filter((v) => validOperators.indexOf(v) >= 0)
+          .forEach((v) => {
+            operators.push(v);
+          });
+      });
+      tempValueString = '';
+      calculateValue();
+      break;
+    default:
+      if (isNumber(sequence) || (sequence == ' ') || validOperators.indexOf(sequence) >= 0) {
+        tempValueString += sequence;
+        process.stdout.write(sequence);
+      } else {
+        process.stdout.write(`${sequence} is not a valid character\n`);
+        process.stdout.write(`${tempValueString}`);
+      }
+      break;
   }
 });
 
-console.log("Application started:");
+console.log('Application started:');
